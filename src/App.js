8a3159508b212ba671466cbc80907fe4e07ef130
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-01-25 14:42:23 +0530

    25-01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
+import styled from 'styled-components';
+
+
+//the name of file and component name should start with an upper case letter always
+
+const Container = styled.div`
+background-color: crimson;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+      <p>asgdhjgas</p>
+      <NavBar/>
+    </Container>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+
+
+
+function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
+//make 3 components which should be functional componenets and integrate those with App.js file
+// and show me the output
+export default NavBar
